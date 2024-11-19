02a99a915cffaa2fbc41b841706547c5b5339071
Krish <krishkumer150@gmail.com>
2024-11-19 17:44:25 +0530

    EmptySpace-bugFix

--- TodoProject/index.js
+++ TodoProject/index.js
@@ -1,2 +1,12 @@
-document.getElementById("text") + "Krish";
-document.title = "Hello-world!";
+const listValue = document.getElementById("list-value");
+const inputBox = document.getElementById("inputValue");
+function callme() {
+  if (inputBox.value.trim() === "") {
+    alert("Plz Enter The Text!");
+    console.log("first-condition!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listValue.appendChild(li);
+  }
+}
